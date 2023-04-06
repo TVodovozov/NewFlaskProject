fac4598dfcb051abe1ac5f5ac11cec0d99332ced
Timur <vodovozzz@inbox.ru>
2023-04-07 01:00:18 +0300

    lesson_3 done

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,29 +1,29 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template
+from flask_login import login_required
+from werkzeug.exceptions import NotFound
+
+from blog.models import User
 
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
-USERS = {
-    1:'Alice',
-    2: 'John',
-    3: 'Mike',
-}
 
 @user.route('/')
 def user_list():
+    users = User.query.all()
     return render_template(
         'users/list.html',
-        users=USERS,
+        users=users,
     )
 
+
 @user.route('/<int:pk>')
-def get_user(pk: int):
-    try:
-        user_name = USERS[pk]
-    except KeyError:
-        # raise NotFound('User id{k} not found')
-        return redirect('/users/')
+@login_required
+def profile(pk: int):
+    selected_user = User.query.filter_by(id=pk).one_or_none()
+    if not selected_user:
+        raise NotFound(f"User #{pk} doesn't exist!")
 
     return render_template(
-        'users/details.html',
-        user_name=user_name,
+        'users/profile.html',
+        user=selected_user,
     )
