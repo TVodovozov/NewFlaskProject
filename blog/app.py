40210d2b438e09dcf36045e3e85d77ca3ba9dfa8
Timur <vodovozzz@inbox.ru>
2023-04-13 23:49:38 +0300

    lesson_6 done

--- blog/app.py
+++ blog/app.py
@@ -1,29 +1,23 @@
-import os
 from flask import Flask
 from blog import commands
-from blog.extensions import db, login_manager, csrf
+from blog.extensions import db, login_manager, csrf, migrate
 from blog.models import User
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
-cfg_name = os.environ.get("CONFIG_NAME") or "DevConfig"
-app.config.from_object(f"blog.configs.{cfg_name}")
-migrate = Migrate(app, db, compare_type=True)
-csrf.init_app(app)
 
 
 def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object('blog.configs')
     register_extensions(app)
     register_blueprints(app)
     register_commands(app)
     return app
 
 
+
 def register_extensions(app):
     db.init_app(app)
+    migrate.init_app(app, db, compare_type=True)
+    csrf.init_app(app)
 
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -36,9 +30,13 @@ def register_extensions(app):
 def register_blueprints(app: Flask):
     from blog.auth.views import auth
     from blog.user.views import user
+    from blog.author.views import author
+    from blog.articles.views import article
 
     app.register_blueprint(user)
     app.register_blueprint(auth)
+    app.register_blueprint(author)
+    app.register_blueprint(article)
 
 
 def register_commands(app: Flask):
